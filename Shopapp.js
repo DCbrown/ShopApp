f0212cd47174e195c3de676af0e6c3d01b706f03
Donovan Brown <Dess5000@gmail.com>
2014-01-29 16:01:30 -0500

    Shop App

---
+++ Shopapp.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+$("#add").click(function() {
+	if ( $.trim( $('#addForm').val() ) === '' ) {
+		
+		return;
+	}
+	else {
+    addItem();
+	}
+});
+
+//when enter button is pressed
+	$("#addForm").keypress(function(e){
+		if(e.which==13){
+			addItem();
+			e.preventDefault();
+		}
+	});
+
+
+//add item
+	function addItem(){
+		var itemToAdd = $("#addForm").val();
+		var createItem = '<div class="add-item"><div class="checkcheckcheck"><input type="checkbox" class="checkbox"></div><input type="text" class="item-field" value=" '+itemToAdd + ' "></div>';
+		$("#shopping-list").append(createItem);
+		$("#addForm").attr("placeholder", "Add an item").val("");
+		$('input.check').change(function(){
+		$(this).siblings('itemToAdd').toggleClass('strike');
+	});
+	}
+
+//remove Item
+	$("#remove").click(function(){
+		$("input:checked").closest(".add-item").fadeOut(500);
+	});
+
+//toggle between line crossed off and on
+	$('#shopping-list').on('change','input[type=checkbox]',function(){
+    	var myInput = $(this).parent().siblings('input[type=text]');
+    	if ($(this).is(':checked')) {
+    		$(myInput).css('textDecoration','line-through');
+    	}
+    	else{
+    		$(myInput).css('textDecoration','none');
+    	}
+});
+	
+		
+});
