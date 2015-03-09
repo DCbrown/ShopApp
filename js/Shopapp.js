916046313a53b55ffe9c31b0ccf634eaa3c5c113
Donovan Brown <Dess5000@gmail.com>
2015-03-09 00:44:15 -0400

    Refactoring code
    Refactoring code

--- js/Shopapp.js
+++ js/Shopapp.js
@@ -9,24 +9,21 @@ $("#add").click(function() {
 	}
 });
 
-//when enter button is pressed
-	$("#addForm").keypress(function(e){
-		if(e.which==13){
-			addItem();
-			e.preventDefault();
-		}
-	});
 
+	
 
 //add item
 	function addItem(){
 		var itemToAdd = $("#addForm").val();
-		var createItem = '<div class="add-item"><div class="checkcheckcheck"><input type="checkbox" class="checkbox"></div><input type="text" class="item-field" value=" '+itemToAdd + ' "></div>';
+		var createItem = 		
+		
+		'<div class="add-item"><div class="checkcheckcheck"><input type="checkbox" class="checkbox"></div><input type="text" class="item-field" value=" '+itemToAdd + ' "></div>';
+		
 		$("#shopping-list").append(createItem);
 		$("#addForm").attr("placeholder", "Add an item").val("");
 		$('input.check').change(function(){
 		$(this).siblings('itemToAdd').toggleClass('strike');
-	});
+		});
 	}
 
 //remove Item
@@ -43,7 +40,7 @@ $("#add").click(function() {
     	else{
     		$(myInput).css('textDecoration','none');
     	}
-});
+	});
 	
 		
 });
